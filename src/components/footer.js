973aa1e0ff009fc7d56c29a241330c8ddf34e5ee
Brett Fouss <bmfouss@gmail.com>
2022-01-14 10:20:53 -0500

    Update information, layout, styles, portfolio

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,22 +1,34 @@
 import React from "react"
+import ButtonLink from "./button-link"
+import { Gatsby } from "@icons-pack/react-simple-icons"
 
 const Footer = () => (
-    <footer>
-        <br />
-            <div className="FooterLinks">
-                <p><strong>Contact, Social Media, Etc.</strong></p>
-                <p><a href="https://docs.google.com/document/d/1SeSeDmjY_F4WU7Ux8bD1PsEnmMvdce1q0UuvOZB1hMY/edit?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a></p>
-                <p><a href="mailto:bmfouss@gmail.com" target="_blank" rel="noopener noreferrer">Email</a></p>
-                <p><a href="https://www.github.com/brettfouss/" target="_blank" rel="noopener noreferrer">Github</a></p>
-                <p><a href="https://www.linkedin.com/in/brett-fouss-697635a9" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
-                <p><a href="https://www.instagram.com/brettfouss/" target="_blank" rel="noopener noreferrer">Instagram</a></p>
-            </div>
-            <div className="FooterLinks">
-                <p>
-                    <strong>Made with <a href="https://www.gatsbyjs.org/" target="_blank" rel="noopener noreferrer">Gatsby</a></strong> © { new Date().getFullYear() } Brett Fouss
-                </p>
-            </div>
-    </footer>
+  <footer>
+    <div>
+      <strong>
+        Made with&nbsp;
+        <span>
+          <Gatsby size={16} />
+          &nbsp;
+          <a
+            href="https://www.gatsbyjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Gatsby
+          </a>
+        </span>
+      </strong>
+      &nbsp;© {new Date().getFullYear()} Brett Fouss
+    </div>
+    <ButtonLink
+      href="https://github.com/brettfouss/brettfouss.com"
+      external={true}
+    >
+      View website source code
+    </ButtonLink>
+    <br />
+  </footer>
 )
 
 export default Footer
---
+++ src/data/portfolio.js
@@ -0,0 +1,75 @@
+const Types = {
+  PROJECT: "project",
+  RESEARCH: "research",
+  WORK: "work",
+}
+const portfolio = [
+  {
+    name: "hubspot-datasets",
+    type: Types.WORK,
+    imagePath: "datasets-portfolio-image.png",
+    link: "https://knowledge.hubspot.com/reports/create-and-use-datasets",
+    title: "HubSpot Datasets",
+    description:
+      "Launched with HubSpot's new enterprise-tier Operations Hub product, Datasets enable the curation and transformation of HubSpot data to streamline analysis.",
+    tags: ["product"],
+    date: new Date(2021, 11, 1),
+  },
+  {
+    name: "hubspot-custom-report-builder",
+    type: Types.WORK,
+    imagePath: "custom-report-builder-portfolio-image.png",
+    link: "https://knowledge.hubspot.com/reports/create-reports-with-the-custom-report-builder",
+    title: "HubSpot Custom Report Builder",
+    description:
+      "Offering a multitude of data sources and visualization options, the new HubSpot custom report builder is a SQL-powered business intelligence tool for everyday users.",
+    tags: ["product"],
+    date: new Date(2021, 2, 1),
+  },
+  {
+    name: "flavor-map",
+    type: Types.PROJECT,
+    link: "https://food.tools",
+    imagePath: "flavor-map-portfolio-image.png",
+    title: "The Flavor Map",
+    description:
+      "Visual exploration of ingredient, pairing, and cuisine data from The Flavor Bible using force simulation.",
+    tags: ["visualization"],
+    date: new Date(2019, 6, 5),
+  },
+  {
+    name: "chinese-vis",
+    type: Types.PROJECT,
+    link: "https://brettfouss.github.io/chinese-vis",
+    imagePath: "chinese-vis-portfolio-image.png",
+    title: "Visualizing the Chinese Language",
+    description:
+      "Narrative visualization piece that examines properties of Chinese characters, and how they relate to one other to form words.",
+    tags: ["visualization"],
+    date: new Date(2019, 4, 18),
+  },
+  {
+    name: "net-set",
+    type: Types.RESEARCH,
+    link: "https://vizsec.org/files/2018/Fouss_Poster.pdf",
+    imagePath: "net-set-poster-abstract-portfolio-image.png",
+    title: "NetSet",
+    description:
+      "Cyber security tool for discovery of correlated network alerts using novel set visualizations. Poster presented at the Symposium on Visualization for Cyber Security at the 2018 IEEE VIS conference in Berlin, Germany.",
+    tags: ["poster", "visualization"],
+    date: new Date(2018, 9, 22),
+  },
+  {
+    name: "punyvis",
+    type: Types.RESEARCH,
+    link: "https://ieeexplore.ieee.org/document/9161590",
+    imagePath: "punyvis-portfolio-image.png",
+    title: "PunyVis",
+    description:
+      "Publication discussing a visual analytics tool for mitigating homograph phishing attacks which exploit visual similarity of Unicode characters to spoof URLs. Presented at the Symposium on Visualization for Cyber Security at the 2019 IEEE VIS conference in Vancouver, Canada.",
+    tags: ["paper", "visualization"],
+    date: new Date(2019, 10, 23),
+  },
+]
+
+export default portfolio
